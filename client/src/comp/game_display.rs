faa498c16887cde33284494974b8bfe0db429f34
Your Name <you@example.com>
2025-03-14 20:14:38 +0200

    holdnext-css

--- client/src/comp/game_display.rs
+++ client/src/comp/game_display.rs
@@ -1,5 +1,5 @@
 use dioxus::prelude::*;
-use game::tet::{CellValue, GameState, Tet};
+use game::tet::{BoardMatrix, BoardMatrixNext, CellValue, GameState, Tet, TetAction};
 
 const INTER_BOX_PADDING: &'static str = "0px";
 const GAMEBOARD_GRID_COLOR: &'static str = "rgb(0, 0, 0)";
@@ -38,37 +38,41 @@ pub fn GameDisplay(game_state: ReadOnlySignal<GameState>) -> Element {
                 justify-content: center;
                 container-type:size;
             ",
+
+            div {
+                style: " 
+                    width: 30%;
+                    height: 100%;
+                    display: flex;
+                    align-items: center;
+                    justify-content: center;
+                ",
             
-            GameDetailsLeftPane {
-                game_state: game_state
             }
 
             div {
-                style: "
-                width: calc(min(100cqw, 50cqh));
-                height: calc(min(100cqh, min(100cqh, 200cqw)));
-                display: flex;
-                align-items: center;
-                justify-content: center;
+                style: " 
+                    width: 40%;
+                    height: 100%;
+                    display: flex;
+                    align-items: center;
+                    justify-content: center;
                 ",
 
-                div {
-                    style: "
-                        padding: {INTER_BOX_PADDING};
-                        width: 100%;
-                        height: 100%;
-                        container-type:size;
-                        border: 3px dashed red;
-                        display: flex;
-                    ",
-                    GameBoardDisplay{
-                        game_state: game_state
-                    }
+                GameDisplayInner {
+                    game_state: game_state
                 }
             }
-            
-            GameDetailsRightPane {
-                game_state: game_state
+
+            div {
+                style: " 
+                    width: 30%;
+                    height: 100%;
+                    display: flex;
+                    align-items: center;
+                    justify-content: center;
+                ",
+
             }
         }
     }
@@ -76,38 +80,215 @@ pub fn GameDisplay(game_state: ReadOnlySignal<GameState>) -> Element {
 
 #[component]
 fn GameDetailsLeftPane(game_state: ReadOnlySignal<GameState>) -> Element {
+    let hold_press = use_memo(move || game_state.read().last_action == TetAction::Hold);
+    let hold_color = use_memo(move || if *hold_press.read() { "rgb(128, 0, 0)" } else { "rgb(0, 0, 0)" });
     rsx! {
         div {
             style: "
-                width: 300px;
+                width: 100%;
                 height: 100%;
-                border: 3px dashed blue;
+                display: flex;
+                align-items: start;
+                justify-content: end;
             ",
+            div {
+                style: "
+                    width: 50%;
+                    font-size: 15cqmin;
+                    font-weight: bold;
+                    text-align: center;
+                    color: {hold_color};
+                ",
+                "Hold"
+            }
+            div {
+                style: "
+                    width: 50%;
+                    align:right;
+                ",
+                GameBoardDisplayHoldGrid {
+                    game_state: game_state
+                }
+            }
         }
     }
 }
 
 #[component]
 fn GameDetailsRightPane(game_state: ReadOnlySignal<GameState>) -> Element {
+    let next_press = use_memo(move || game_state.read().last_action == TetAction::HardDrop );
+    let next_color = use_memo(move || if *next_press.read() { "rgb(128, 0, 0)" } else { "rgb(0, 0, 0)" });
     rsx! {
         div {
             style: "
-                width: 300px;
+                width: 100%;
                 height: 100%;
-                border: 3px dashed green;
+                display: flex;
+                align-items: start;
+                justify-content: start;
             ",
+            div {
+                style: "
+                    width: 50%;
+                    margin-bottom: auto;
+                    align:left;
+                ",
+                GameBoardDisplayNextGrid {
+                    game_state: game_state
+                }
+            }
+            div {
+                style: "
+                    width: 50%;
+                    font-size: 15cqmin;
+                    font-weight: bold;
+                    text-align: center;
+                    color: {next_color};
+                ",
+                "Next"
+            }
         }
     }
 }
 
 
+#[component]
+fn GameDisplayInner(game_state: ReadOnlySignal<GameState>) -> Element {
+    rsx! {
+        div {
+            style: "
+                width: 100%;
+                height: 100%;
+                display: flex;
+                align-items: center;
+                justify-content: center;
+                container-type:size;
+            ",
+            div {
+                style: "
+                    position: relative;
+                    width: calc(min(100cqw, 50cqh));
+                    height: calc(min(100cqh, min(100cqh, 200cqw)));
+                    display: flex;
+                    align-items: center;
+                    justify-content: center;
+                    container-type:size;
+                ",
+                div {
+                    style: "
+                    position:absolute;
+                        top: 0; 
+                        left: -74cqw;
+                        width: 73cqw;
+                        height: 99cqh;
+                    ",
+                    GameDetailsLeftPane {
+                        game_state: game_state
+                    }
+                }
+
+                div {
+                    style: "
+                        padding: {INTER_BOX_PADDING};
+                        width: 100%;
+                        height: 100%;
+                        container-type:size;
+                        display: flex;
+                    ",
+                    GameBoardDisplayMainGrid{
+                        game_state: game_state
+                    }
+                }
+                div {
+                    style: "
+                    position:absolute;
+                        top: 0; 
+                        left: 101cqw;
+                        width: 73cqw;
+                        height: 99cqh;
+                    ",
+                    GameDetailsRightPane {
+                        game_state: game_state
+                    }
+                }
+            }
+
+        }
+    }
+}
 
 
 #[component]
-fn GameBoardDisplay(game_state: ReadOnlySignal<GameState>) -> Element {
-    let column_count = 10;
-    let row_count = 20;
+fn GameBoardDisplayMainGrid(game_state: ReadOnlySignal<GameState>) -> Element {
+    let main_board = use_memo(move || game_state.read().main_board);
+    rsx! {
+        BoardGrid {
+            board: main_board
+        }
+    }
+}
 
+#[component]
+fn GameBoardDisplayNextGrid(game_state: ReadOnlySignal<GameState>) -> Element {
+    let next_board = use_memo(move || game_state.read().get_next_board());
+    rsx! {
+        BoardGrid {
+            board: next_board
+        }
+    }
+}
+#[component]
+fn GameBoardDisplayHoldGrid(game_state: ReadOnlySignal<GameState>) -> Element {
+    let hold_board = use_memo(move || game_state.read().get_hold_board());
+    rsx! {
+        BoardGrid {
+            board: hold_board
+        }
+    }
+}
+
+
+#[component]
+fn BoardGrid<const R: usize, const C: usize>(board: ReadOnlySignal<BoardMatrix<R, C>>) -> Element {
+    let column_count = C as i8;
+    let row_count = (R as i8).min(20);
+    rsx! {
+        GameBoardGridParent {
+            column_count: column_count,
+            row_count: row_count,
+            children: rsx! {
+                for row_id in 0..row_count {
+                    for col_id in 0..column_count {
+                        BoardGridCell {
+                            board: board,
+                            row: (row_count - 1 - row_id) as i8,
+                            col: col_id as i8,
+                            row_count: row_count,
+                            col_count: column_count
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+#[component]
+fn BoardGridCell<const R: usize, const C: usize>(board: ReadOnlySignal<BoardMatrix<R, C>>, row: i8, col: i8, row_count: i8, col_count: i8) -> Element {
+    let cell = use_memo(move || board.read().get_cell(row, col));
+    rsx! {
+        GridCellDisplay {
+            cell: cell,
+            row: row,
+            col: col,
+            row_count: row_count,
+            col_count: col_count
+        }
+    }
+}
+
+#[component]
+fn GameBoardGridParent(column_count: i8, row_count: i8, children: Element) -> Element {
     rsx! {
         div {
             style: "
@@ -122,34 +303,24 @@ fn GameBoardDisplay(game_state: ReadOnlySignal<GameState>) -> Element {
                 background-color:{GAMEBOARD_GRID_COLOR};
                 padding: {INTER_BOX_PADDING};
                 border: 1px solid {GAMEBOARD_GRID_COLOR};
+                aspect-ratio: {column_count}/{row_count};
             ",
-
-            for row_id in 0..row_count {
-                for col_id in 0..column_count {
-                    GridCell {
-                        game_state: game_state,
-                        row: (row_count - 1 - row_id) as i8,
-                        col: col_id as i8
-                    }
-                }
-            }
+            {children}
         }
     }
 }
 
 #[component]
-fn GridCell(game_state: ReadOnlySignal<GameState>, row: i8, col: i8) -> Element {
-    let cell_value = use_memo(move || game_state.read().main_board.get_cell(row, col));
-    let cell_color = use_memo(move || get_cell_color(cell_value.read().clone()));
-
+fn GridCellDisplay(cell: ReadOnlySignal<Option<CellValue>>, row: i8, col: i8, row_count: i8, col_count: i8) -> Element {
+    let cell_color = use_memo(move || get_cell_color(cell.read().clone()));
     rsx! {
         div {
             style: "
                 posiition: absolute;
-                width: calc(100cqw/10-{INTER_BOX_PADDING});
-                height: calc(100cqh/20-{INTER_BOX_PADDING});
-                top: calc((100cqh/20-{INTER_BOX_PADDING}) * {row});
-                left: calc((100cqw/10-{INTER_BOX_PADDING}) * {col});
+                width: calc(100cqw/{col_count}-{INTER_BOX_PADDING});
+                height: calc(100cqh/{row_count}-{INTER_BOX_PADDING});
+                top: calc((100cqh/{row_count}-{INTER_BOX_PADDING}) * {row});
+                left: calc((100cqw/{col_count}-{INTER_BOX_PADDING}) * {col});
                 padding: {INTER_BOX_PADDING};
             ",
             div {
@@ -158,7 +329,7 @@ fn GridCell(game_state: ReadOnlySignal<GameState>, row: i8, col: i8) -> Element 
                 width: 100%;
                 height: 100%;
                 aspect-ratio: 1/1;
-                border: 1px solid {GAMEBOARD_GRID_COLOR};
+                border: 0.1cqmin solid {GAMEBOARD_GRID_COLOR};
                 "
             }
         }
--- client/src/pages/homepage.rs
+++ client/src/pages/homepage.rs
@@ -1,29 +1,35 @@
-use std::time::Duration;
+use std::{collections::VecDeque, time::Duration};
 
 use crate::comp::game_display::*;
 use crate::route::Route;
 use dioxus::prelude::*;
 use dioxus_sdk::utils::timing::use_interval;
-use game::tet::{GameState, TetAction};
+use game::{bot::{wordpress_blog_bot::WordpressBlogBot, TetBot}, tet::{GameState, TetAction}};
 use dioxus::logger::tracing::info;
 /// Home page
 #[component]
 pub fn Home() -> Element {
     let mut game_state = use_signal(GameState::empty);
+    let mut pending_actions = use_signal(VecDeque::<TetAction>::new);
     use_interval(Duration::from_secs_f32(0.1), move || {
         let mut g = game_state.write();
+        let mut p = pending_actions.write();
         
         if g.game_over() {
             *g = GameState::empty();
-        } else {
-            for _ in 0..10 {
-                if let Ok(new_state) = g.try_action(TetAction::random(), 0) {
-                    *g = new_state;
-                    break;
-                }
+            return;
+        }
+        if p.is_empty() {
+            if let Ok(r) = WordpressBlogBot.choose_move(&g) {
+                *p = VecDeque::from_iter(r.into_iter());
             }
         }
 
+        if let Some(a) = p.pop_front() {
+            if let Ok(new_state) = g.try_action(a, 0) {
+                *g = new_state;
+            }
+        }
     });
 
     rsx! {
