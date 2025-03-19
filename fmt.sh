b6eb598e6b0bce38e9cb05e68c61a8d4d430ee69
johnny-smitherson <127537716+johnny-smitherson@users.noreply.github.com>
2025-03-19 10:38:07 +0200

    fmt-script

---
+++ fmt.sh
@@ -0,0 +1,32 @@
+set -ex
+
+if [ -z "$(git status --porcelain)" ]; then 
+  # Working directory clean
+  
+  (
+    # dx fmt -s
+    cargo fmt
+  )
+
+    git add .
+    git status
+    git commit -m "cargo fmt $(date)" || true
+    git push
+
+    (  cargo fix   --allow-dirty || true   )
+    
+    git add .
+    git status
+    git commit -m "cargo fix $(date)" || true
+    git push
+
+else 
+    git status
+    set +x
+    echo
+    echo "!!!"
+    echo
+    echo "WORKING DIRECTORY NOT CLEAN"
+    echo "PLZ COMMIT CHANGES"
+    exit 66
+fi
