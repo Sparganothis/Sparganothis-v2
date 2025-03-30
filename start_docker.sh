aa01e593526b0f650aaf9ffcd7ed6e495b2581bd
johnny-smitherson <127537716+johnny-smitherson@users.noreply.github.com>
2025-03-30 23:52:21 +0200

    docker compose

--- start_docker.sh
+++ start_docker.sh
@@ -2,6 +2,6 @@
 set -ex
 export MSYS_NO_PATHCONV=1
 cd _docker
-docker-compose rm -f  || true
-docker-compose up -d --remove-orphans
-docker-compose logs -f
+docker compose rm -f  || true
+docker compose up -d --remove-orphans
+docker compose logs -f
